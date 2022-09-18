d79a899c6d5200fdb218ce78ce34f80c04f91210
dekork <deniskrkt@gmail.com>
2022-09-18 15:12:11 -0700

    Fixed db init connection settings when table is non-existent.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,24 @@
 var mysql      = require('mysql');
 const fs = require("fs");
 if (process.env.NODE_ENV === 'development') {
+    var tempconnection = mysql.createConnection({
+        host: 'localhost',
+        user: 'swimlane',
+        password: 'passw0rd',
+        multipleStatements: true
+    });
+    tempconnection.query(fs.readFileSync("config/init.sql").toString(), function(err, res) {
+        if (err === null)
+            console.log("Database initialized successfully.");
+        else
+        {
+            console.log("Database failed to init...");
+            console.log(err);
+        }
+    })
+    tempconnection.end(function(err){
+        console.log('Finishing database init. Errors:' + (err) ? err : "None.");
+    });
     var connection = mysql.createPool({
         host: 'localhost',
         user: 'swimlane',
@@ -8,17 +26,28 @@ if (process.env.NODE_ENV === 'development') {
         database: 'swimlane',
         multipleStatements: true
     });
-    connection.query(fs.readFileSync("config/init.sql").toString(), function(err, res) {
+}
+else {
+    console.log("Initializing db using production DB: " + process.env.MYSQL_USER + " @ " + process.env.MYSQL_HOST)
+    var tempconnection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        multipleStatements: true
+    });
+    tempconnection.query(fs.readFileSync("config/init.sql").toString(), function(err, res) {
         if (err === null)
             console.log("Database initialized successfully.");
         else
         {
-            console.log("Database failed to init...")
+            console.log("Database failed to init...");
+            console.log(err);
         }
     })
-}
-else {
-    console.log("using production DB: " + process.env.MYSQL_USER + " @ " + process.env.MYSQL_HOST)
+    tempconnection.end(function(err){
+        console.log('Finishing database init.');
+    });
+
     var connection = mysql.createPool({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
@@ -26,14 +55,7 @@ else {
         database: 'swimlane',
         multipleStatements: true
     });
-    connection.query(fs.readFileSync("config/init.sql").toString(), function(err, res) {
-        if (err === null)
-            console.log("Database initialized successfully.");
-        else
-        {
-            console.log("Database failed to init...");
-        }
-    })
+
 
 }
 module.exports = connection;
