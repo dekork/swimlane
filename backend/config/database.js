983bbb0054c588591eb847364a65acf886bdebe8
dekork <deniskrkt@gmail.com>
2022-09-18 14:46:52 -0700

    Cleanup database logging (also, excuse to repush to ECS)

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,6 +18,7 @@ if (process.env.NODE_ENV === 'development') {
     })
 }
 else {
+    console.log("using production DB: " + process.env.MYSQL_USER + " @ " + process.env.MYSQL_HOST)
     var connection = mysql.createPool({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
@@ -31,8 +32,6 @@ else {
         else
         {
             console.log("Database failed to init...");
-            console.log(process.env.MYSQL_HOST);
-            console.log(process.env.MYSQL_USER);
         }
     })
 
